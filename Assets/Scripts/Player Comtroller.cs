21d32eb7538b3580baadec080e8e79a565ec46d3
AlekSukhoy <149101516+AlekSukhoy@users.noreply.github.com>
2023-10-27 03:41:03 +0300

    Adding movement WASD
    NEW META, MOVEMENT

--- Assets/Scripts/Player Comtroller.cs
+++ Assets/Scripts/Player Comtroller.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerComtroller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("player is moving");
-    }
-
+    public GameObject Player;
+    public float Speed = 2.0f;
     // Update is called once per frame
     void Update()
     {
-        
+
+
+
+        //Перемещаем наш объект на S расстояние по оси x.
+        if (Input.GetKey(KeyCode.W))
+        {
+            Player.transform.Translate(0, 0, Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Player.transform.Translate(Speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Player.transform.Translate(0, 0, -Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Player.transform.Translate(-Speed * Time.deltaTime, 0, 0);
+        }
     }
 }
